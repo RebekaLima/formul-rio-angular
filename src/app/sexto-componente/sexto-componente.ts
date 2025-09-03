df1466edb9bd9a4524317b426384d50c67c54c9c
Rebeka Lima <rebeka.silva.emailprofissional@gmail.com>
2025-09-02 21:40:01 -0300

    initial commit

---
+++ src/app/sexto-componente/sexto-componente.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-sexto-componente',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './sexto-componente.html',
+  styleUrl: './sexto-componente.css'
+})
+export class SextoComponente {
+
+  nome: string='';
+  cidade: string='';
+
+
+}
